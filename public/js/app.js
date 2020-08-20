2b7d645b906dd28c8465bf4e799679cb0ca4ed21
Roger Gomez <roger@techknob.com>
2020-08-19 21:37:09 -0400

    ready for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading ...'
     messageTwo.textContent = ''
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     fetch(url).then((response) => {
     response.json().then((data) => {
         if (data.error) {
